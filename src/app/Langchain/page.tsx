cda98b179af9b9f6d6d81c01a899db189b15f152
deshantk332720 <deshantk332720@gmail.com>
2025-08-02 11:23:53 +0500

    push my code

--- src/app/Langchain/page.tsx
+++ src/app/Langchain/page.tsx
@@ -1,16 +1,16 @@
-
+'use client'
 export default function AIDifferenceTable() {
     return (
       <div
         className="min-h-screen bg-cover bg-center text-white"
         style={{ backgroundImage: "url('/30.png')" }}
       >
-         <div ></div>
-         <h1  className="bg-gradient-to-br from-blue-900 via-slate-900 to-black border border-blue-800 rounded-2xl p-6 shadow-xl  transition-all duration-300 text-xl font-bold text-blue-300 mb-3 text-center justify-center mt-70">First, we'll understand the OpenAI SDK — the foundation for building intelligent agents.
+         <div className="bg-gradient-to-br from-blue-900 via-slate-900 to-black border border-blue-800 rounded-2xl p-6 shadow-xl  transition-all duration-300 text-xl font-bold text-blue-300 mb-3 text-center justify-center mt-70">
+         <h1  >First, we will understand the OpenAI SDK — the foundation for building intelligent agents.
 <p>Then, we'll explore CrewAI and LangChain to build more advanced and autonomous agent systems.</p>
 </h1>
   
-         
+</div>
         
       </div>
     );
